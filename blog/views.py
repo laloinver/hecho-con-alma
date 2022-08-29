b0e297f69def9bde9d34f7e45ee5e4a87acc3c46
Gerardo Invernizzi <laloinvernizzi@gmail.com>
2022-08-28 22:44:14 -0300

    cambios en el blog (lista, ABM, comentarios)

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from .forms import PostForm
 from .models import Post, Comments
 from .forms import CommentForm
 
-
 # Create your views here.
 
 def post_list(request):
@@ -13,51 +12,60 @@ def post_list(request):
     print(posts.query)
     return render(request, 'blog/blog_list.html', {'posts': posts})
 
-
 def post_create(request):
-    # si es una petición get, es decir, si se visita la 
-    # página de creación de post por primera vez
-    form = PostForm() # instanciamos el formulario sin datos previos
-    if request.method == 'POST':
-        # si es una petición post, es decir, el usuario rellenó los campos
-        # del formulario
-        # cargamos los datos enviados por petición post al formulario
-        # acá, de esta manera, notar que como tiene campo con archivos, esos van en request.FILES
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            # si es válido el formulario creamos el objeto post pero no lo guardamos en db: commit=False
-            post = form.save(commit=False)
-            post.user = request.user # asignamos el autor del post
-            post.save() # guardamos
-            messages.success(request, 'Post creado con éxito') # mandamos un mensaje de éxito
-            return redirect('post_list') # dirigimos al usuario a la página que nos parezca
-        messages.error(request, 'Hay errores en el formulario')
-    return render(request, 'blog/blog_create.html', {'form': form})
-
+    user = request.user
+    if user.groups.filter(name='creadorPost').exists():
+        # si es una petición get, es decir, si se visita la 
+        # página de creación de post por primera vez
+        form = PostForm() # instanciamos el formulario sin datos previos
+        if request.method == 'POST':
+            # si es una petición post, es decir, el usuario rellenó los campos
+            # del formulario
+            # cargamos los datos enviados por petición post al formulario
+            # acá, de esta manera, notar que como tiene campo con archivos, esos van en request.FILES
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                # si es válido el formulario creamos el objeto post pero no lo guardamos en db: commit=False
+                post = form.save(commit=False)
+                post.user = request.user # asignamos el autor del post
+                post.save() # guardamos
+                # messages.success(request, 'Post creado con éxito') # mandamos un mensaje de éxito
+                return redirect('post_list') # dirigimos al usuario a la página que nos parezca
+            messages.error(request, 'Hay errores en el formulario')
+        return render(request, 'blog/blog_create.html', {'form': form})
+    else:
+        return redirect('post_list')
+    endif
 
 def post_update(request, pk):
-    # recibimos ese argumento 'pk' enviado como parte de la url:
-    # path('update/<int:pk>/', views.post_update, name='post_update'), es pk es el que recibimos como argumento
-    post = get_object_or_404(Post, id=pk) # buscamos el post o devolvemos un 404 (not found)
+    user = request.user
+    if user.groups.filter(name='creadorPost').exists():
+        # recibimos ese argumento 'pk' enviado como parte de la url:
+        # path('update/<int:pk>/', views.post_update, name='post_update'), es pk es el que recibimos como argumento
+        post = get_object_or_404(Post, id=pk) # buscamos el post o devolvemos un 404 (not found)
 
-    # instanciamos el formulario e inicializamos los campos con los datos del post encontrado (instance=post)
-    form = PostForm(instance=post)
-    if request.method == 'POST':
-        # si es una petición post, es decir, el usuario está guardando el formulario
-        # rellenamos con los datos en POST y FILES
-        form = PostForm(request.POST, request.FILES, instance=post)
-        if form.is_valid():
-            # si es válido, guardamos el formulario, se guarda la instancia, de hecho
-            form.save()
-            return redirect('post_list') # redirigimos
-    return render(request, 'blog/blog_update.html', {'form': form, 'post': post})
+        # instanciamos el formulario e inicializamos los campos con los datos del post encontrado (instance=post)
+        form = PostForm(instance=post)
+        if request.method == 'POST':
+            # si es una petición post, es decir, el usuario está guardando el formulario
+            # rellenamos con los datos en POST y FILES
+            form = PostForm(request.POST, request.FILES, instance=post)
+            if form.is_valid():
+                # si es válido, guardamos el formulario, se guarda la instancia, de hecho
+                form.save()
+                return redirect('post_list') # redirigimos
+        return render(request, 'blog/blog_update.html', {'form': form, 'post': post})
+    else:
+        return redirect('post_list')
+    endif
 
 def post_delete(request, pk):
-    # buscamos el post y lo borramos, lanzamos un error 404 (Not found caso contrario)
-    post = get_object_or_404(Post, id=pk)
-    post.delete()
+    user = request.user
+    if user.groups.filter(name='creadorPost').exists():
+        # buscamos el post y lo borramos, lanzamos un error 404 (Not found caso contrario)
+        post = get_object_or_404(Post, id=pk)
+        post.delete()
     return redirect('post_list')
-
 
 def post_detail(request, pk):
     # buscamos el post y lo mostramos
