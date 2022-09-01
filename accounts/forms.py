551aa5760097ee6b6dadd58ce56adbd59e752e94
ezequieljm <ezequieljm020@gmail.com>
2022-09-01 12:21:44 -0300

    Finish home, login, logout and register users

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ from accounts.models import User
 class LoginForm(forms.Form):
     # username = forms.CharField(label="Usuario", max_length=50)
     # password = forms.CharField(label="Contraseña", max_length=50, widget=forms.PasswordInput)
-    username = forms.CharField(widget=forms.TextInput(), max_length=50)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'w3-input'}), max_length=50)
     password = forms.CharField(widget=forms.PasswordInput(), max_length=50, )
 
 
